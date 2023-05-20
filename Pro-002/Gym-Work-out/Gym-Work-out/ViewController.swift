9c0f7cf5c297d955e27b6a1c11eaa92c7405d562
pamuditha <pamuditha.lak@gmail.com>
2023-05-20 21:09:27 +0530

    initial-commit-1

---
+++ Pro-002/Gym-Work-out/Gym-Work-out/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gym-Work-out
+//
+//  Created by Maleesha Wijeratne on 18/05/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Pro-002/Gym-Work-out/createAccountViewController.swift
@@ -0,0 +1,49 @@
+//
+//  createAccountViewController.swift
+//  Gym-Work-out
+//
+//  Created by Maleesha Wijeratne on 18/05/2023.
+//
+
+import UIKit
+import Firebase
+
+class createAccountViewController: UIViewController {
+
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func submitBtnclick(_ sender: UIButton) {
+        
+        
+        guard let email = emailTextField.text else{return}
+        guard let password = passwordTextField.text else{return}
+        
+        Auth.auth().createUser(withEmail: email, password: password) { firebaseResult, error in
+            if let e = error{
+                let alert = UIAlertController(title: "Somthing went wrong", message: "Please check the Email and password", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                self.present(alert, animated: true, completion: nil)
+            }
+            else{
+                self.performSegue(withIdentifier: "goToNext", sender: self)
+            }
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
